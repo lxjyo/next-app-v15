46aa768d1190fa0f7b323460ff502189ed20301a
lxjyo <lxj665294@gmail.com>
2026-06-22 18:24:30 +0800

    feat:请求

--- src/app/01.tsx
+++ src/app/01.tsx
@@ -1,16 +1,16 @@
-import Card from '@/components/Card';
+
 // server request 推荐使用服务端请求，更安全、更高效
 import { getUserRequestKey } from '@/utils/config';
 
 export default async function Page() {
   const res = await fetch(`${process.env.NEXT_BASE_URL}/posts?key=${getUserRequestKey().key}`);
   const data = await res.json();
-  return <Card data={JSON.stringify(data)} />;
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 // client request  
 // 'use client'
-// import { getUserRequestKey } from '@/utils/config'; 
+// import { getUserRequestKey } from '@/utils/config';
 // import { useState, useEffect } from 'react';
 // export default function Page() {
 //   const [state, setState] = useState(null);
---
+++ src/utils/config.ts
@@ -0,0 +1,10 @@
+import "server-only";
+// 防止泄露，比如server-only标记的数据不能作为数据传递给客户端
+import { experimental_taintUniqueValue } from "react";
+export function getUserRequestKey() {
+  const data = {
+    key: "123_4564",
+  };
+  experimental_taintUniqueValue("Do not share this key", data, data.key);
+  return data;
+}
