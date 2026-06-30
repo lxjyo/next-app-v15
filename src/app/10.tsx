84f4cc8e094d51378ccf75c10d56529de4dab47c
lxjyo <lxj665294@gmail.com>
2026-06-30 17:50:22 +0800

    prisma

--- src/app/10.tsx
+++ src/app/10.tsx
@@ -1,5 +1,17 @@
-import prisma from '../lib/prisma';
+import { neon } from '@neondatabase/serverless'
+const sql = neon(`${process.env.DATABASE_URL}`)
 
+// 查
+
+
+// // 增
+// const result = await sql('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password])
+
+// // 改
+// const result = await sql('UPDATE users SET username = $1 WEHRE id = 1', [username])
+
+// // 删
+// const result = await sql('DELETE FROM users WHERE id = $1', [id])
 
 /**
  * 开发环境 每次刷新页面都会生成一个新的随机数
@@ -10,17 +22,12 @@ import prisma from '../lib/prisma';
 
 import { revalidatePath } from 'next/cache'
 export default async function Page() {
- const result = await prisma.user.findMany();
+ const result = await sql`SELECT * FROM users`;
  async function createAction(formData: FormData) {
   'use server'
   const username = formData.get('username');
   const password = formData.get('password');
-  await prisma.user.create({
-    data: {
-      username: username as string,
-      password: password as string,
-    },
-  });
+  await sql`INSERT INTO users (username, password) VALUES (${username}, ${password})`;
   revalidatePath('/');
  }
   return <div className="text-amber-600">
